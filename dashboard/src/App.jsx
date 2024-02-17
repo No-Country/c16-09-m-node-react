f17074ffb2be65aa8cc8adf4e94e869593a3a590
GeraZorzoni <gzorzoni@gmail.com>
2024-02-17 14:20:28 -0300

    Configuro componente de login y configuro rutas

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,12 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Dashboard />
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='login/' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ dashboard/src/components/Login.jsx
@@ -0,0 +1,20 @@
+import { useForm } from "react-hook-form";
+
+export default function Login() {
+  const { register, handleSubmit } = useForm();
+
+  const enviar = (data) => {
+    console.log("enviado", data);
+  };
+
+  return (
+    <div className=''>
+      <h1 className=''>Ingresa a tu cuenta</h1>
+      <form className='' onSubmit={handleSubmit(enviar)}>
+        <input type='email' placeholder='Ingresa tu Email' {...register("email")} />
+        <input type='password' placeholder='Ingresa tu password' {...register("password")} />
+        <button type='submit'>Enviar</button>
+      </form>
+    </div>
+  );
+}
