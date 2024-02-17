c74c870f8dce3f7d4cc471e7a54eaaa2a83e0493
GeraZorzoni <gzorzoni@gmail.com>
2024-02-17 13:28:49 -0300

    agrego botones para registro de comercio, registro de usuario, login, y una maquetado de la search nav bar

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -1,7 +1,14 @@
 function Dashboard() {
   return (
     <>
+      <button> Registro Comercio</button>
+      <button> Registro Usuario</button>
+      <button>Ingresa a tu cuenta</button>
       <h1>encuentraprecio.com</h1>
+      <form>
+        <input type='search' placeholder='Search' />
+        <button type='submit'>Search</button>
+      </form>
     </>
   );
 }
