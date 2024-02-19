b454a26d8427f505b210463d2834258fb4ca542f
unknown <sdjsm2003@gmail.com>
2024-02-19 15:39:55 -0400

    Fixing associations, part1

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,12 +1,32 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config({ path: "./.env" });
 const { BD_NAME, PASSWORD, USER_NAME } = process.env;
+const fs = require('fs');
+const path = require('path')
 
 const sequelize = new Sequelize(BD_NAME, USER_NAME, PASSWORD, {
   host: "localhost",
   dialect: "mysql",
   port: 3306,
 });
+
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach(model => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
 
 const { Users, Products, Commerce, Rating, Pay } = sequelize.models;
 
