8fcb94b0036f2e77af1ace57d3bc08a38cabb61b
GeraZorzoni <gzorzoni@gmail.com>
2024-02-20 15:24:56 -0300

    Ordeno carpetas wireframe para mejorar orden de las carpetas del proyecto

--- dashboard/src/components/CommerceRegister.jsx
+++ dashboard/src/components/CommerceRegister.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-
 export default function CommerceRegister() {
   const [formData, setFormData] = useState({
     shopName: "",
@@ -16,9 +15,9 @@ export default function CommerceRegister() {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-  
+
   const handleCheckboxChange = () => {
-    setTermsAccepted(!termsAccepted); 
+    setTermsAccepted(!termsAccepted);
   };
 
   const handleSubmit = (e) => {
@@ -27,12 +26,9 @@ export default function CommerceRegister() {
       alert("Debe aceptar los términos y condiciones.");
       return;
     }
-   
-      resetForm();
-    
+
+    resetForm();
   };
-
-
 
   const resetForm = () => {
     setFormData({
@@ -47,122 +43,89 @@ export default function CommerceRegister() {
   };
 
   return (
-    <body className="page-commerce">
-     
-        <div className="form-container">
-          <form onSubmit={handleSubmit}>
-          <div className="form-group">
+    <body className='page-commerce'>
+      <div className='form-container'>
+        <form onSubmit={handleSubmit}>
+          <div className='form-group'>
             <h2>Datos del Comercio</h2>
           </div>
-            <div className="form-group">
-              <label>Nombre del Comercio:
-
-                <input
-                  type="text"
-                  name="shopName"
-                  value={formData.shopName}
-                  onChange={handleChange}
-                  required
-                />
-              </label>
-            </div>
-            <div className="form-group">
-              <label>Domicilio:
-
-                <input
-                  type="text"
-                  name="address"
-                  value={formData.address}
-                  onChange={handleChange}
-                  placeholder="Calle y número"
-                  required
-                />
-              </label>
-            </div>
-            <div className="form-group">
-              <label>Provincia:
-
-                <input
-                  type="text"
-                  name="province"
-                  value={formData.province}
-                  onChange={handleChange}
-                  required
-                />
-              </label>
-            </div>
-            <div className="form-group">
-              <label>Localidad:
-
+          <div className='form-group'>
+            <label>
+              Nombre del Comercio:
+              <input type='text' name='shopName' value={formData.shopName} onChange={handleChange} required />
+            </label>
+          </div>
+          <div className='form-group'>
+            <label>
+              Domicilio:
               <input
-                type="text"
-                name="city"
-                value={formData.city}
+                type='text'
+                name='address'
+                value={formData.address}
+                onChange={handleChange}
+                placeholder='Calle y número'
+                required
+              />
+            </label>
+          </div>
+          <div className='form-group'>
+            <label>
+              Provincia:
+              <input type='text' name='province' value={formData.province} onChange={handleChange} required />
+            </label>
+          </div>
+          <div className='form-group'>
+            <label>
+              Localidad:
+              <input type='text' name='city' value={formData.city} onChange={handleChange} required />
+            </label>
+          </div>
+
+          <div className='form-group'>
+            <label>
+              Email:
+              <input type='email' name='email' value={formData.email} onChange={handleChange} required />
+            </label>
+          </div>
+          <div className='form-group'>
+            <label>
+              Password:
+              <input
+                type='password'
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+                pattern='(?=.*\d)(?=.*[A-Z]).{8,}'
+                title='Password debe contener al menos 8 caracteres incluyendo un número y una mayúscula'
+                required
+              />
+            </label>
+          </div>
+          <div className='form-group'>
+            <label>
+              Confirmar Password:
+              <input
+                type='password'
+                name='confirmPassword'
+                value={formData.confirmPassword}
                 onChange={handleChange}
                 required
               />
-              </label>
-            </div>
+            </label>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='termsCheckbox'>
+              Acepto los términos y condiciones
+              <input type='checkbox' id='termsCheckbox' checked={termsAccepted} onChange={handleCheckboxChange} />
+            </label>
+          </div>
 
-            <div className="form-group">
-              <label>Email:
-
-                <input
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-              </label>
-            </div>
-            <div className="form-group">
-              <label>Password:
-
-                <input
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  pattern="(?=.*\d)(?=.*[A-Z]).{8,}"
-                  title="Password debe contener al menos 8 caracteres incluyendo un número y una mayúscula"
-                  required
-                />
-              </label>
-            </div>
-            <div className="form-group">
-              <label>Confirmar Password:
-
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  value={formData.confirmPassword}
-                  onChange={handleChange}
-                  required
-                />
-              </label>
-            </div>
-            <div className="form-group">
-              <label htmlFor="termsCheckbox">
-                Acepto los términos y condiciones
-
-                <input
-                  type="checkbox"
-                  id="termsCheckbox"
-                  checked={termsAccepted}
-                  onChange={handleCheckboxChange}
-                />
-              </label>
-            </div>
-
-            <div className="form-group">
-              <button type="submit">Registrar</button>
-              <button type="button">
-                Salir
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className='form-group'>
+            <button type='submit'>Registrar</button>
+            <button type='button'>Salir</button>
+          </div>
+        </form>
+      </div>
     </body>
   );
 }
