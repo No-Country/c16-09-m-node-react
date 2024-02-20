6a71fff746bc43353cfa80b74c69789d2fbd5aed
GeraZorzoni <gzorzoni@gmail.com>
2024-02-20 15:33:27 -0300

    Corrijo anidamiento de body dentro de div

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -4,31 +4,30 @@ function Dashboard() {
   return (
     <>
       <header>
-          <button>
-            <Link to='/registrocomercio'>Registro Comercio</Link>
-          </button>
-          <button>
-            <Link to='/registrousuario'>Registro Usuario</Link>
-          </button>
-          <button>
-            <Link to='/login'>Ingresa a tu cuenta</Link>
-          </button>
-        </header>
-      <body className="dashboard">
-        <h1 className="title">encuentraprecio.com</h1>
-        <div className="parrafo">
-
-          <p>Únete a nosotros en encuentraprecio.com 
-          para una revolución en las compras localizadas.
-          Pequeños negocios, grandes minoristas y compradores astutos se reúnen aquí,
-          donde los productos son listados por todos,
-          ¡y las mejores ofertas están a solo un clic de distancia!</p>
+        <button>
+          <Link to='/registrocomercio'>Registro Comercio</Link>
+        </button>
+        <button>
+          <Link to='/registrousuario'>Registro Usuario</Link>
+        </button>
+        <button>
+          <Link to='/login'>Ingresa a tu cuenta</Link>
+        </button>
+      </header>
+      <div className='dashboard'>
+        <h1 className='title'>encuentraprecio.com</h1>
+        <div className='parrafo'>
+          <p>
+            Únete a nosotros en encuentraprecio.com para una revolución en las compras localizadas. Pequeños negocios,
+            grandes minoristas y compradores astutos se reúnen aquí, donde los productos son listados por todos, ¡y las
+            mejores ofertas están a solo un clic de distancia!
+          </p>
         </div>
         <form>
           <input type='search' placeholder='Search' />
           <button type='submit'>Search</button>
         </form>
-      </body>
+      </div>
     </>
   );
 }
