9ff8b5c7964a927de32f565b20e8d731e575b23f
SH-ur <sdjsm2003@gmail.com>
2024-02-21 12:27:45 -0400

    Registro de usuario funcional

--- Backend/src/routes/users.js
+++ Backend/src/routes/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require("../controllers/userController")
-
+const userController = require("../controllers/userController");
+const userCreateController = require("../controllers/userCreateController");
 /**
  * @swagger
  * tags:
@@ -23,6 +23,40 @@ const userController = require("../controllers/userController")
  *         description: Error del servidor
  */
 
-router.get("/users", userController.index);
+/**
+ * @swagger
+ * /user/register:
+ *   post:
+ *     summary: Registrar datos de usuario
+ *     tags: [Users]
+ * requestBody:
+ * required: true
+ * content:
+ * application/json:
+ * schema:
+ * type: object
+ * properties:
+ * Name: string
+ * last_name: string
+ * dni: integer
+ * date_of_birth: dateonly
+ * mail: string
+ * location: string
+ * province: string
+ * logged_in: boolean
+ * password: string
+ * phone_number: string
+ *     responses:
+ *       200:
+ *         description: Un objeto el usuario creado
+ *       500:
+ *         description: Error del servidor
+ * 400:
+ * description: Error al procesar datos o usuario ya registrado previamente
+ */
+
+router
+  .get("/users", userController.index)
+  .post("/register", userCreateController);
 
 module.exports = router;
