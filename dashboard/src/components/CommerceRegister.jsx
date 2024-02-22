c318d546ab9a01e6793ad917dcbc6eceb321ead1
GeraZorzoni <gzorzoni@gmail.com>
2024-02-22 06:48:56 -0300

    Agrego link para volver a inicio

--- dashboard/src/components/CommerceRegister.jsx
+++ dashboard/src/components/CommerceRegister.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function CommerceRegister() {
@@ -126,6 +126,7 @@ export default function CommerceRegister() {
             <button type='button'>Salir</button>
           </div>
         </form>
+        <Link to='/'>Volver a inicio</Link>
       </div>
     </body>
   );
