47ca69af3949a6a5defb83ecda052fff6d1779b8
GeraZorzoni <gzorzoni@gmail.com>
2024-02-26 18:16:10 -0300

    Corrijo formato del componente login

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -39,32 +39,17 @@ export default function Login() {
   };
 
   return (
-
-    <div className="login">
-      <div className="div-login">
-        <h1 className="title">encuentraprecio.com</h1>
-        <h1 className="sub-title">Ingresa a tu cuenta</h1>
-        <form
-          className="form-container-login"
-          onSubmit={handleSubmit(send)}
-        >
-          <input
-            className="input-login"
-            type="email"
-            placeholder="Ingresa tu Email"
-            {...register("email")}
-          />
-          <input
-            className="input-login"
-            type="password"
-            placeholder="Ingresa tu password"
-            {...register("password")}
-          />
-          <button type="submit">Enviar</button>
-          <Link to="/">Volver a inicio</Link>
+    <div className='login'>
+      <div className='div-login'>
+        <h1 className='title'>encuentraprecio.com</h1>
+        <h1 className='sub-title'>Ingresa a tu cuenta</h1>
+        <form className='form-container-login' onSubmit={handleSubmit(send)}>
+          <input className='input-login' type='email' placeholder='Ingresa tu Email' {...register("email")} />
+          <input className='input-login' type='password' placeholder='Ingresa tu password' {...register("password")} />
+          <button type='submit'>Enviar</button>
+          <Link to='/'>Volver a inicio</Link>
         </form>
       </div>
-
     </div>
   );
-
+}
