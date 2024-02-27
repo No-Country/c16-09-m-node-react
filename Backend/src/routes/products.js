bab48701fc524c2e7ae6d3895518d816d5b280e9
Daniel Rojas <cdrojas.2018@gmail.com>
2024-02-27 19:07:19 -0300

    add-multer

--- Backend/src/routes/products.js
+++ Backend/src/routes/products.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path')
 
 const productController = require('../controllers/productController');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, '../public/uploads')
+    },
+    filename: function(req, file, cb){
+        let nameOfFile = 'img-product-' + Date.now() + path.extname(file.originalname)
+        cb(null, nameOfFile)
+    },
+});
+
+const upload = multer({ storage: storage });
 
 /**
  * @swagger
@@ -24,9 +38,9 @@ const productController = require('../controllers/productController');
  */
 
 router.get('/List', productController.read);
-router.post('/create', productController.create);
+router.post('/create', upload.single('image'), productController.create);
 router.delete('/delete/:id', productController.softDelete);
 router.get('/restore/:id', productController.restore)
-router.put('/update/:id', productController.update)
+router.put("/update/:id", upload.single("image"), productController.update);
 
 module.exports = router;
