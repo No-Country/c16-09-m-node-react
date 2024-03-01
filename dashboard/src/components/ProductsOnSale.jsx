b1f044e28178e31b5bc532d39762cb10acd87938
GeraZorzoni <gzorzoni@gmail.com>
2024-03-01 15:02:40 -0300

    finalizo trabajo sobre productos OnSale

--- dashboard/src/components/ProductsOnSale.jsx
+++ dashboard/src/components/ProductsOnSale.jsx
@@ -1,35 +1,33 @@
-import axios from "axios";
 import ProductCard from "./ProductCard";
 import "./ProductsMostViewed.css";
 import { useEffect, useState } from "react";
 
 export default function ProductsOnSale() {
-  const [products, setProducts] = useState([]);
   const url = "http://localhost:8000/products/List";
 
-  const getProducts = async () => {
-    try {
-      const response = await axios.get(url);
-      const filteredProducts = response.data.filter((product) => product.offers);
-      setProducts(filteredProducts);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [productsOnSale, setproductsOnSale] = useState([]);
 
   useEffect(() => {
-    getProducts();
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const result = await response.json();
+      const onSale = result.filter((product) => product.offers === true);
+      setproductsOnSale(onSale);
+    };
+    fetchData();
   }, []);
 
   return (
     <div>
       <h1>Productos en Oferta</h1>
       <div className='product-card-container'>
-        {products.map((product) => (
-          <div key={product.id} className='product-card'>
-            <ProductCard product={product} />
-          </div>
-        ))}
+        {productsOnSale.map((product) => {
+          return (
+            <div key={product.id} className='product-card'>
+              <ProductCard product={product} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
