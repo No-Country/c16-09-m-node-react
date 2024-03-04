7e447eb250234269de80515b3eae7fa46b7e91a2
GeraZorzoni <gzorzoni@gmail.com>
2024-03-04 09:53:15 -0300

    trabajo sobre productos recien agregados

--- dashboard/src/components/ProductsMostViewed.jsx
+++ dashboard/src/components/ProductsMostViewed.jsx
@@ -4,7 +4,7 @@ import "./ProductsMostViewed.css";
 export default function MostViewedProducts() {
   return (
     <div>
-      <h1> Sector Productos mas Buscados</h1>
+      <h1> Sector Productos Recien agregados</h1>
       {/* <div className='product-card-container'>
         <div className='product-card'>
           <ProductCard />
