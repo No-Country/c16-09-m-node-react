508315917fb40d7021af0d7b7a7fec26f103aa7c
SH-ur <sdjsm2003@gmail.com>
2024-03-09 14:20:17 -0400

    colocando sync en index.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -80,16 +80,6 @@ server.use((err, req, res, next) => {
   console.error(err);
   res.status(status).send(message);
 });
-if(process.env.DEPLOYMENT_ON == 'YES'){
-  server.listen(process.env.MYSQLPORT || 8000, () => {
-    console.log(`Listening on port ${process.env.MYSQLPORT || 8000}`);
-  });  
-} else{
-  console.log("hola estamos en el port " + process.env.PORT)
-  server.listen(process.env.PORT || 8000, () => {
-    console.log(`Listening on port ${process.env.PORT || 8000}`);
-  });
-}
 
 
 module.exports = server;
